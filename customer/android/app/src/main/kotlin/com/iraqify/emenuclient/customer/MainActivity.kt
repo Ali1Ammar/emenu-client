3f22e9c0c343d151e5dad3bb98ab0054a46e6745
Ali <aliammarali125@gmail.com>
2022-01-15 22:57:21 +0300

    init

---
+++ customer/android/app/src/main/kotlin/com/iraqify/emenuclient/customer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.iraqify.emenuclient.customer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ manger/android/app/src/main/kotlin/com/iraqify/emenumanger/manger/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.iraqify.emenumanger.manger
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
