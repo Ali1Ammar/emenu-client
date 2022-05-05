6aef9c18ce7bfe920bad245c788bb33be8f41890
Ali <aliammarali125@gmail.com>
2022-05-06 01:31:04 +0300

    create FluentCheckBox

---
+++ manger/lib/shared/widget/flyount_checkbox.dart
@@ -0,0 +1,81 @@
+import 'package:fluent_ui/fluent_ui.dart';
+
+class FluentCheckBox<T> extends StatefulWidget {
+  final List<FluentCheckBoxItem<T>> items;
+  final ValueChanged<List<T>> onChangedSelected;
+  final Widget Function(BuildContext context, VoidCallback opener) buttonBuilder;
+  const FluentCheckBox(
+      {Key? key, required this.items, required this.onChangedSelected,required this.buttonBuilder})
+      : super(key: key);
+
+  @override
+  State<FluentCheckBox> createState() => _FluentCheckBoxState();
+}
+
+class _FluentCheckBoxState<T> extends State<FluentCheckBox<T>> {
+  final flyoutController = FlyoutController();
+  final checkedSet = <T>{};
+  @override
+  void initState() {
+    for (var element in widget.items) {
+      if (element.checked) {
+        checkedSet.add(element.value);
+      }
+    }
+
+    flyoutController.addListener(() {
+      if(flyoutController.isClosed){
+        widget.onChangedSelected(checkedSet.toList());
+      }
+    });
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    flyoutController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Flyout(
+      controller: flyoutController,
+      content: (context) {
+        return StatefulBuilder(
+          builder: ((context, setState) => FlyoutContent(
+              constraints: const BoxConstraints(maxWidth: 200),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: widget.items
+                    .map((e) => Checkbox(
+                          checked: checkedSet.contains(e.value),
+                          content: e.child,
+                          onChanged: (val) {
+                            setState(() {
+                              if (val!) {
+                                checkedSet.add(e.value);
+                              } else {
+                                checkedSet.remove(e.value);
+                              }
+                            });
+                          },
+                        ))
+                    .toList(),
+              ))),
+        );
+      },
+      child: widget.buttonBuilder(context,flyoutController.open),
+    );
+  }
+}
+
+class FluentCheckBoxItem<T> {
+  final Widget child;
+  final bool checked;
+  final T value;
+
+  FluentCheckBoxItem(this.child, this.checked, this.value);
+}
